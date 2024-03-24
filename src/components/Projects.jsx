4ee5009868460eea5943f8315ed60508018006a8
Eric Bartsch <ericbartsch@Erics-MacBook-Air.local>
2024-03-24 09:10:58 -0600

    update projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,35 +1,39 @@
+import songsWithFriends from '../assets/images/projects/songs-with-friends.png';
 import codingPractice from '../assets/images/projects/coding-practice.png';
 import hangulPractice from '../assets/images/projects/hangul-practice.png';
 import koreanVocab from '../assets/images/projects/korean-vocab.png';
-import netflix from '../assets/images/projects/netflix.png';
 
 import { useState } from "react";
 
 function Projects() {
   const projectsList = [
     {
+      name: "Songs with Friends",
+      decsription: "A multiplayer song guessing game that utilizes web sockets and web speech API",
+      image: songsWithFriends,
+      url: "https://songs-with-friends-client-production.up.railway.app/"
+    },
+    {
       name: "Coding Practice",
       decsription: "Tool created to practice algorithm problems, featuring an interactive terminal utilizing codemirror and React",
-      image: codingPractice
+      image: codingPractice,
+      url: "https://monumental-blancmange-86349e.netlify.app/"
     },
     {
       name: "Korean Vocab",
       decsription: "Flash card-like app to learn the most common words in the Korean language. Features Korean-English toggle and speech for Korean",
-      image: koreanVocab
-    },
-    {
-      name: "Netflix Clone",
-      decsription: "Netflix clone using themoviedb as an api source for top movies and tv shows, built using Vue",
-      image: netflix
+      image: koreanVocab,
+      url: "https://soft-kulfi-47eb0f.netlify.app"
     },
     {
       name: "Hangul Practice",
       decsription: "Tool to practice Hangul (Korean writing) utilizing Web Speech API to access text to speech, HTML Canvas to capture user drawn image, and Google Cloud Vision API as an OCR to compare the user drawn image to the correct Korean text",
-      image: hangulPractice
+      image: hangulPractice,
+      url: null
     },
   ];
 
-  const [showProjectDetailsList, setShowProjectDetailsList] = useState([]);
+  const [showProjectDetailsList, setShowProjectDetailsList] = useState([0]);
 
   const toggleShowProjectDetail = (projectIndex) => {
     const updatedShowProjectDetailsList = [...showProjectDetailsList];
@@ -64,8 +68,10 @@ function Projects() {
                   </span>
                   <span>)</span>
                   {showProjectDetailsList.includes(index) && 
-                    <div>
-                      <img src={project.image} />
+                    <div className="py-[15px]">
+                      <a href={project.url} target="_blank">
+                        <img className={`${project.url && "cursor-pointer"}`} src={project.image} />
+                      </a>
                     </div>
                   }
                 </div>
